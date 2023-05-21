50f8a9696e35705f7f28635be19f785bbd74dcf3
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2023-05-21 13:27:39 +0300

    Add files via upload

---
+++ Assignment2/FractionCalculator.h
@@ -0,0 +1,18 @@
+#ifndef FRACTIONCALCULATOR_H
+#define FRACTIONCALCULATOR_H
+#include <iostream>
+using namespace std;
+
+class FractionCalculator
+{
+    public:
+        FractionCalculator();
+        virtual ~FractionCalculator();
+        void calculator();
+
+    protected:
+
+    private:
+};
+
+#endif // FRACTIONCALCULATOR_H
